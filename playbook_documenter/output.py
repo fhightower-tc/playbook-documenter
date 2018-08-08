5bfcc24f892f828247fe276dec2e382cd95cfa0b
Floyd Hightower <floyd.hightower27@gmail.com>
2018-08-08 06:08:08 -0700

    Adding test, example pbs

--- playbook_documenter/output.py
+++ playbook_documenter/output.py
@@ -25,63 +25,59 @@ def create_markdown_output(json_docs):
 {% endif %}
 
 {%- if json_docs.http_link -%}
-    ### HTTP Link Trigger
+### HTTP Link Trigger
 
-    *This playbook uses an HTTP trigger. The following parts of the request sent to trigger this playbook are used in the playbook:*
+*This playbook uses an HTTP trigger. The following apps use some part of the incoming request:*
 
-    {% for request_part in json_docs.http_link -%}
-        - {{ request_part.app }}: {{ request_part.part }}
-    {% endfor %}
+{% for request_part in json_docs.http_link -%}
+- {{ request_part.app }}: {{ request_part.part }}
+{% endfor %}
 {% endif %}
 
 {%- if json_docs.custom_metrics -%}
-    ### Custom Metrics
+### Custom Metrics
 
-    *This playbook uses the following custom metrics \[[learn more](https://docs.threatconnect.com/en/latest/rest_api/custom_metrics/custom_metrics.html#custom-metrics)\]:*
-
-    {% for custom_metric in json_docs.custom_metrics -%}
-        - Custom Metric Name: {{ custom_metric.name }}
-            - Weight: {{ custom_metric.weight }}
-            - Date: {{ custom_metric.date }}
-            - Value: {{ custom_metric.value }}
-            - Key: {{ custom_metric.key }}
-    {% endfor %}
+*This playbook uses the following custom metrics \[[learn more](https://docs.threatconnect.com/en/latest/rest_api/custom_metrics/custom_metrics.html#custom-metrics)\]:*
+{% for custom_metric in json_docs.custom_metrics %}
+- Custom Metric Name: {{ custom_metric.name }}
+  - Weight: {{ custom_metric.weight }}
+  - Date: {{ custom_metric.date }}
+  - Value: {{ custom_metric.value }}
+  - Key: {{ custom_metric.key }}
+{% endfor %}
 {% endif %}
 
 {%- if json_docs.datastores -%}
-    ### Datastores
+### Datastores
 
-    *This playbook uses the following datastores:*
-
-    {% for datastore in json_docs.datastores -%}
-        <!-- TODO: only display a value is it is not empty -->
-        - `{{ datastore.db_method }} {{ datastore.domain_type.lower() }}/{{ datastore.type_name }}/{{ datastore.path }}`:
-            - Datastore Entity: `{{ datastore.request_entity }}`
-            - Datastore Organization: {{ datastore.organization_name }}
-    {% endfor %}
-
+*This playbook uses the following datastores:*
+{% for datastore in json_docs.datastores %}
+- `{{ datastore.db_method }} {{ datastore.domain_type.lower() }}/{{ datastore.type_name }}/{{ datastore.path }}`:
+  - {% if datastore.request_entity %} Datastore Entity: `{{ datastore.request_entity }}`{% endif %}
+  - Datastore Organization: {{ datastore.organization_name }}
+{% endfor %}
 {% endif %}
 
 {%- if json_docs.variables|length > 0 -%}
-    {% if json_docs.variables.user_variables|length > 0 %}
-        ### User Variables
+{% if json_docs.variables.user_variables|length > 0 %}
+### User Variables
 
-        *This playbook expects the following user variables:*
+*This playbook expects the following user variables:*
 
-        {% for variable in json_docs.variables.user_variables -%}
-            - {{ variable.type }}{% if variable.name %}{{ variable.name }}{% else %}<NO NAME GIVEN>{% endif %}
-        {% endfor %}
-    {% endif %}
+{% for variable in json_docs.variables.user_variables -%}
+- {{ variable.type }}{% if variable.name %}{{ variable.name }}{% else %}<NO NAME GIVEN>{% endif %}
+{% endfor %}
+{% endif %}
 
-    {% if json_docs.variables.org_variables|length > 0 %}
-        ### Organization Variables
+{% if json_docs.variables.org_variables|length > 0 %}
+### Organization Variables
 
-        *This playbook expects the following organization variables:*
+*This playbook expects the following organization variables:*
 
-        {% for variable in json_docs.variables.org_variables -%}
-            - {{ variable.type }}{% if variable.name %}{{ variable.name }}{% else %}<NO NAME GIVEN>{% endif %}
-        {% endfor %}
-    {% endif %}
+{% for variable in json_docs.variables.org_variables -%}
+- {{ variable.type }}{% if variable.name %}{{ variable.name }}{% else %}<NO NAME GIVEN>{% endif %}
+{% endfor %}
+{% endif %}
 {% endif %}
 
 {%- if json_docs.internal_variables -%}
@@ -90,7 +86,7 @@ def create_markdown_output(json_docs):
 *The following variables are declared in this playbook:*
 
 {% for variable in json_docs.internal_variables -%}
-  - {{ variable.key }}: {% if variable.value %}{{ variable.value }}{% else %}<NO VALUE GIVEN>{% endif %}
+- {{ variable.key }}: {% if variable.value %}{{ variable.value }}{% else %}<NO VALUE GIVEN>{% endif %}
 {% endfor %}
 {% endif %}
 """
--- tests/test_playbook_documenter.py
+++ tests/test_playbook_documenter.py
@@ -10,8 +10,8 @@ import pytest
 from playbook_documenter import playbook_documenter
 
 
-def _read_file():
-    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), "./data/pb1.pbx")), 'r') as f:
+def _read_file(file_name='add_tag_attribute.pbx'):
+    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), './data/' + file_name)), 'r') as f:
         return f.read()
 
 
@@ -28,10 +28,9 @@ def test_bad_output_format():
         docs = playbook_documenter.generate_documentation(pb_string, output_format='foo')
 
 
-def test_markdown_output():
+def test_markdown_output_1():
     pb_string = _read_file()
     docs = playbook_documenter.generate_documentation(pb_string, output_format='markdown')
-    print("docs {}".format(docs))
     assert docs.strip() == """## Documentation
 
 ### Triggers
@@ -44,3 +43,133 @@ def test_markdown_output():
 
 - groupID: #Trigger:1044:trg.action.item!String
 - metadataAddingPlaybookTrigger: <NO VALUE GIVEN>"""
+
+
+def test_markdown_output_2():
+    pb_string = _read_file('page_monitor.pbx')
+    docs = playbook_documenter.generate_documentation(pb_string, output_format='markdown')
+    assert docs.strip() == """## Documentation
+
+### Triggers
+
+- Timer
+
+### Datastores
+
+*This playbook uses the following datastores:*
+
+- `GET local/pageMonitor/`:
+  - 
+  - Datastore Organization: 
+
+- `POST local/pageMonitor/#App:10146:databaseID!String`:
+  -  Datastore Entity: `{"hash": "#App:10129:hash.encode.md5!String"}`
+  - Datastore Organization: 
+
+### Variables Declared in the Playbook
+
+*The following variables are declared in this playbook:*
+
+- userAgent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+- slackChannel: #slack-channel
+- website: http://example.com
+- slackMessagePrefix: [Page Monitor PB]
+- nonExistHash: 0"""
+
+
+def test_markdown_output_3():
+    pb_string = _read_file('URL_reader.pbx')
+    docs = playbook_documenter.generate_documentation(pb_string, output_format='markdown')
+    assert docs.strip() == """## Documentation
+
+### Triggers
+
+- HttpLink
+
+### HTTP Link Trigger
+
+*This playbook uses an HTTP trigger. The following apps use some part of the incoming request:*
+
+- HTTP Client 2: body"""
+
+
+def test_markdown_output_4():
+    pb_string = _read_file('RF Alerts Incident Create.pbx')
+    docs = playbook_documenter.generate_documentation(pb_string, output_format='markdown')
+    assert docs.strip() == """## Documentation
+
+### Triggers
+
+- CVE
+- Task
+
+### Variables Declared in the Playbook
+
+*The following variables are declared in this playbook:*
+
+- RF: Recorded Future Alert
+- XRFToken: your_api_key_here"""
+
+
+def test_markdown_output_5():
+    pb_string = _read_file('Array Serializer.pbx')
+    docs = playbook_documenter.generate_documentation(pb_string, output_format='markdown')
+    print("{}".format(docs))
+    assert docs.strip() == """## Documentation
+
+### Triggers
+
+- HttpLink
+
+### HTTP Link Trigger
+
+*This playbook uses an HTTP trigger. The following apps use some part of the incoming request:*
+
+- Json Path 1: body
+- Find and Replace 1: body
+- Value Lookup 1: queryparam
+- Logger 1: body
+
+### Variables Declared in the Playbook
+
+*The following variables are declared in this playbook:*
+
+- triggerForThisPlaybook: https://sandbox.threatconnect.com/api/playbook/402b2179-b9a1-4085-9e6b-2604fb015b3d
+- errorMessage: Request to #App:14487:link!String failed: #App:14488:http.status_code!String . Make sure this link is correct and that the playbook related to this link is turned on.
+- errorMessage: Request to #App:14482:triggerForThisPlaybook!String failed: #App:14486:http.status_code!String . Make sure this link is correct (it should be the trigger link for this playbook)."""
+
+
+def test_markdown_output_6():
+    pb_string = _read_file('Recurring Task.pbx')
+    docs = playbook_documenter.generate_documentation(pb_string, output_format='markdown')
+    print("{}".format(docs))
+    assert docs.strip() == """## Documentation
+
+### Triggers
+
+- Timer
+
+### Variables Declared in the Playbook
+
+*The following variables are declared in this playbook:*
+
+- taskName: [RECURRING] Send Metrics to Administrator
+- dueDateOffset: 2D"""
+
+
+def test_markdown_output_7():
+    pb_string = _read_file('indicator comment link creator.pbx')
+    docs = playbook_documenter.generate_documentation(pb_string, output_format='markdown')
+    print("{}".format(docs))
+    assert docs.strip() == """## Documentation
+
+### Triggers
+
+- UserAction
+
+### Variables Declared in the Playbook
+
+*The following variables are declared in this playbook:*
+
+- errorMessagePrefix: [Indictor Comment Link Creator PB]:
+- shareCommentLink: [[#Trigger:1066:trg.action.type!String:#Trigger:1066:trg.action.item!String]]"""
